160f44d1c0322f9bec7290065d7bb7aa49a73ce5
Dmytro Kotykhin <kotykhin_d@ukr.net>
2024-09-12 15:19:54 +0200

    change query provider

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -2,31 +2,39 @@
 
 import { ReactNode } from 'react';
 
-import { isServer, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { isServer, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60 * 1000,
-      },
-    },
-  });
-}
+// function makeQueryClient() {
+//   return new QueryClient({
+//     defaultOptions: {
+//       queries: {
+//         staleTime: 60 * 1000,
+//       },
+//     },
+//   });
+// }
 
-let browserQueryClient: QueryClient | undefined = undefined;
+// let browserQueryClient: QueryClient | undefined = undefined;
 
-function getQueryClient() {
-  if (isServer) {
-    return makeQueryClient();
-  } else {
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
-}
+// function getQueryClient() {
+//   if (isServer) {
+//     return makeQueryClient();
+//   } else {
+//     if (!browserQueryClient) browserQueryClient = makeQueryClient();
+//     return browserQueryClient;
+//   }
+// }
+
+// export const QueryProvider = ({ children }: { children: ReactNode }) => {
+//   const queryClient = getQueryClient();
+
+//   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+// };
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export const QueryProvider = ({ children }: { children: ReactNode }) => {
-  const queryClient = getQueryClient();
+  const queryClient = new QueryClient();
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
