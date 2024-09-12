68c61ccbc488a5029c853d8b3d8852679445908e
Dmytro Kotykhin <kotykhin_d@ukr.net>
2024-09-12 15:36:27 +0200

    return query provider updates

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -2,39 +2,31 @@
 
 import { ReactNode } from 'react';
 
-// import { isServer, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { isServer, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-// function makeQueryClient() {
-//   return new QueryClient({
-//     defaultOptions: {
-//       queries: {
-//         staleTime: 60 * 1000,
-//       },
-//     },
-//   });
-// }
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
 
-// let browserQueryClient: QueryClient | undefined = undefined;
+let browserQueryClient: QueryClient | undefined = undefined;
 
-// function getQueryClient() {
-//   if (isServer) {
-//     return makeQueryClient();
-//   } else {
-//     if (!browserQueryClient) browserQueryClient = makeQueryClient();
-//     return browserQueryClient;
-//   }
-// }
-
-// export const QueryProvider = ({ children }: { children: ReactNode }) => {
-//   const queryClient = getQueryClient();
-
-//   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
-// };
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
 
 export const QueryProvider = ({ children }: { children: ReactNode }) => {
-  const queryClient = new QueryClient();
+  const queryClient = getQueryClient();
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
