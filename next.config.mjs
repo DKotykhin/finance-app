ce17df247e2a1f9da42cef0492ea26091d0f8460
Dmytro Kotykhin <kotykhin_d@ukr.net>
2024-09-18 11:09:38 +0200

    next config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000', 'finance-app-sandy-two.vercel.app'],
-    },
-  },
+  // experimental: {
+  //   serverActions: {
+  //     allowedOrigins: ['localhost:3000', 'finance-app-sandy-two.vercel.app'],
+  //   },
+  // },
 };
 
 export default nextConfig;
