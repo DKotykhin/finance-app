f9b90fc2f95f56eb6f971db03020412582b7017b
Dmytro Kotykhin <kotykhin_d@ukr.net>
2024-10-14 23:31:47 +0200

    restore transaction store

--- src/app/(user-pages)/transactions/TransactionList.tsx
+++ src/app/(user-pages)/transactions/TransactionList.tsx
@@ -17,7 +17,7 @@ import {
 } from '@nextui-org/react';
 import { EyeIcon, Loader2, Pencil, Trash2, TriangleAlert } from 'lucide-react';
 import { toast } from 'react-toastify';
-import { format, subDays, isToday } from 'date-fns';
+import { differenceInDays, format, subDays, isToday } from 'date-fns';
 import { useUser } from '@clerk/nextjs';
 import {
   Selection,
@@ -29,7 +29,7 @@ import {
   TableCell,
   getKeyValue,
 } from '@nextui-org/react';
-import { parseAbsoluteToLocal } from '@internationalized/date';
+import { DateValue, parseAbsoluteToLocal } from '@internationalized/date';
 import { SortOrder, UserSettings } from '@prisma/client';
 
 import {
@@ -41,7 +41,7 @@ import {
 import { getAccounts } from '@/actions/Account/_index';
 import { getCategories } from '@/actions/Category/_index';
 import { useConfirm } from '@/hooks/use-confirm';
-import { cn, currencyMap, numberWithSpaces, rowsPerPageArray, valueToDate } from '@/utils/_index';
+import { cn, currencyMap, dateToValue, numberWithSpaces, rowsPerPageArray, valueToDate } from '@/utils/_index';
 import { useTransactionsStore } from '@/store/transactionsSlice';
 
 import { TransactionModal } from './TransactionModal';
@@ -72,12 +72,11 @@ export const TransactionList: React.FC<TransactionListProps> = ({
   userSettingsData,
   isUserSettingsLoading,
 }) => {
-  const { accountValue, setAccountValue, categoryValue, setCategoryValue, dateValue, setDateValue } =
+  const { accountValue, categoryValue, startDate, endDate, setAccountValue, setCategoryValue, setStartDate, setEndDate } =
     useTransactionsStore();
 
   const [updateTransaction, setUpdateTransaction] = useState<TransactionUpdate | null>(null);
   const [transaction, setTransaction] = useState<ExtendedTransaction | null>(null);
-
   const [selectedKeys, setSelectedKeys] = useState<Selection>(new Set());
   const [sortDescriptor, setSortDescriptor] = useState<SortDescriptor>({
     column: userSettingsData?.transactionSortField || 'date',
@@ -87,26 +86,33 @@ export const TransactionList: React.FC<TransactionListProps> = ({
   const [pages, setPages] = useState(1);
   const [transactionListLength, setTransactionListLength] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(userSettingsData?.transactionRowsPerPage || '5');
+  // to do: move to store
+  const [dateValue, setDateValue] = useState<{ start: DateValue; end: DateValue }>({
+    start: parseAbsoluteToLocal(subDays(new Date(), 30).toISOString()),
+    end: parseAbsoluteToLocal(new Date().toISOString()),
+  });
 
   const { user } = useUser();
-  const queryClient = useQueryClient();
-  
   const { isOpen: isOpenView, onOpen: onOpenView, onOpenChange: onOpenChangeView } = useDisclosure();
   const { isOpen: isOpenUpdate, onOpen: onOpenUpdate, onOpenChange: onOpenChangeUpdate } = useDisclosure();
 
   const periodInDays = useMemo(() => {
-    return valueToDate(dateValue.end)?.getDate() - valueToDate(dateValue.start)?.getDate();
+    return differenceInDays(valueToDate(dateValue.end), valueToDate(dateValue.start));
   }, [dateValue]);
 
   useEffect(() => {
-    if (!dateValue.start) {
-      setDateValue({
-        ...dateValue,
+    if (startDate) {
+      setDateValue((v) => ({ ...v, start: dateToValue(startDate) }));
+    } else {
+      setDateValue((v) => ({
+        ...v,
         start: parseAbsoluteToLocal(subDays(new Date(), (userSettingsData?.transactionPeriod ?? 30) - 1).toISOString()),
-      });
+      }));
     }
-    if (!dateValue.end) {
-      setDateValue({ ...dateValue, end: parseAbsoluteToLocal(new Date().toISOString()) });
+    if (endDate) {
+      setDateValue((v) => ({ ...v, end: dateToValue(endDate) }));
+    } else {
+      setDateValue((v) => ({ ...v, end: parseAbsoluteToLocal(new Date().toISOString()) }));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userSettingsData?.transactionPeriod]);
@@ -126,6 +132,8 @@ export const TransactionList: React.FC<TransactionListProps> = ({
     title: 'Delete Transaction',
     message: 'Are you sure you want to delete this transaction?',
   });
+
+  const queryClient = useQueryClient();
 
   const { data: accountData, isLoading: isAccountLoading } = useQuery({
     enabled: !!user?.id,
@@ -200,6 +208,15 @@ export const TransactionList: React.FC<TransactionListProps> = ({
   const onSelectedKeys = (keys: Selection) => {
     setSelectedKeys(keys);
     selectedKeysFn(keys === 'all' ? tableContent?.map((transaction) => transaction.id) : Array.from(keys));
+  };
+
+  const onChangeStartDateValue = (value: DateValue) => {
+    setDateValue((v) => ({ ...v, start: value }));
+    setStartDate(valueToDate(value));
+  };
+  const onChangeEndDateValue = (value: DateValue) => {
+    setDateValue((v) => ({ ...v, end: value }));
+    setEndDate(valueToDate(value));
   };
 
   const tableContent = useMemo(() => {
@@ -337,7 +354,7 @@ export const TransactionList: React.FC<TransactionListProps> = ({
                     label="Date from"
                     granularity="day"
                     value={dateValue.start}
-                    onChange={(value) => setDateValue({ ...dateValue, start: value })}
+                    onChange={onChangeStartDateValue}
                     className="w-full lg:w-[160px]"
                     isDisabled={isTransactionLoading}
                   />
@@ -345,7 +362,7 @@ export const TransactionList: React.FC<TransactionListProps> = ({
                     label="Date to"
                     granularity="day"
                     value={dateValue.end}
-                    onChange={(value) => setDateValue({ ...dateValue, end: value })}
+                    onChange={onChangeEndDateValue}
                     className="w-full lg:w-[160px]"
                     isDisabled={isTransactionLoading}
                   />
@@ -353,9 +370,7 @@ export const TransactionList: React.FC<TransactionListProps> = ({
               )}
             </div>
             <p className="lg:hidden text-xs italic text-gray-400 mt-1 ml-1">
-              {isToday(valueToDate(dateValue.end))
-                ? `last ${periodInDays + 1} days selected`
-                : `${periodInDays + 1} days selected`}
+              {isToday(valueToDate(dateValue.end)) ? `last ${periodInDays + 1} days selected` : `${periodInDays + 1} days selected`}
             </p>
           </div>
           <div className="flex flex-col sm:flex-row gap-2 items-center w-full lg:w-auto">
@@ -404,9 +419,7 @@ export const TransactionList: React.FC<TransactionListProps> = ({
       </div>
       {!isUserSettingsLoading && (
         <p className="hidden lg:block text-xs italic text-gray-400 mb-3 ml-1 mt-1">
-          {isToday(valueToDate(dateValue.end))
-            ? `last ${periodInDays + 1} days selected`
-            : `${periodInDays + 1} days selected`}
+          {isToday(valueToDate(dateValue.end)) ? `last ${periodInDays + 1} days selected` : `${periodInDays + 1} days selected`}
         </p>
       )}
     </>
@@ -421,7 +434,7 @@ export const TransactionList: React.FC<TransactionListProps> = ({
         color="secondary"
         page={page}
         total={pages}
-        onChange={setPage}
+        onChange={(page) => setPage(page)}
       />
     </div>
   );
--- src/store/transactionsSlice.ts
+++ src/store/transactionsSlice.ts
@@ -1,28 +1,25 @@
 /* eslint-disable no-unused-vars */
 import { create } from 'zustand';
 import { Selection } from '@nextui-org/react';
-import { DateValue } from '@internationalized/date';
 
 type TransactionsState = {
   accountValue: Selection;
   categoryValue: Selection;
-  dateValue: {
-    start: DateValue;
-    end: DateValue;
-  }
+  startDate?: Date;
+  endDate?: Date;
   setAccountValue: (value: Selection) => void;
   setCategoryValue: (value: Selection) => void;
-  setDateValue: (value: { start: DateValue; end: DateValue }) => void;
+  setStartDate: (date: Date) => void;
+  setEndDate: (date: Date) => void;
 };
 
 export const useTransactionsStore = create<TransactionsState>()((set) => ({
   accountValue: new Set(['all']),
   categoryValue: new Set(['all']),
-  dateValue: {
-    start: null as unknown as DateValue,
-    end: null as unknown as DateValue,
-  },
+  startDate: undefined,
+  endDate: undefined,
   setAccountValue: (value: Selection) => set(() => ({ accountValue: value })),
   setCategoryValue: (value: Selection) => set(() => ({ categoryValue: value })),
-  setDateValue: (dateValue: { start: DateValue; end: DateValue }) => set(() => ({ dateValue })),
+  setStartDate: (startDate: Date) => set(() => ({ startDate })),
+  setEndDate: (endDate: Date) => set(() => ({ endDate })),
 }));
